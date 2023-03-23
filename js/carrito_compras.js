5c39ef8a3c837e52c3b9c7edc6896442c1ae987b
TatianaVargasD <tatianavd03@gmail.com>
2023-03-23 12:04:40 -0500

    nuevo

---
+++ js/carrito_compras.js
@@ -0,0 +1,6 @@
+let imgPrincipal =document.getElementById("imgPrincipal");
+let mainBox =document.getElementById("mmainBox")
+
+imgPrincipal.addEventListener("click",function(){
+    mainBox.classList.add("fondo-oscuro")
+});
