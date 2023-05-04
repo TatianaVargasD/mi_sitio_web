d8035c60b4752ab51ec96bb7f027cc6cddce4cb8
luiscar05 <penaabellaluiscarlos@gmail.com>
2023-05-03 20:05:38 -0500

    movimiento_img

--- js/carrito_compras.js
+++ js/carrito_compras.js
@@ -109,3 +109,260 @@ imgBox.addEventListener("click",function () {
 });
 
 
+
+let boxImgSecond1=document.getElementById("boxImgSecond1")
+let boxImgSecond2=document.getElementById("boxImgSecond2")
+let boxImgSecond3=document.getElementById("boxImgSecond3")
+let boxImgSecond4=document.getElementById("boxImgSecond4")
+let imgMainLigh=document.getElementById("imgMainLigh")
+/* let imagenes=["img/image-product-1.jpg","img/image-product-2.jpg","img/image-product-3.jpg","img/image-product-4.jpg"]
+let contador=0
+let sombras=0
+function carrusel(contenedor) {
+    contenedor.addEventListener("click", e=>{
+        let ConAdelante=contenedor.querySelector('.adelante'),
+        ConAtras=contenedor.querySelector('.atras'),
+        img=contenedor.querySelector('.img-main-ligh'),
+        fls=e.target;
+
+        if (fls==ConAtras) {
+            if (contador>0) {
+                img.src=imagenes[contador-1]
+                contador-1
+            }else{
+                img.src=imagenes[contador.length-1]
+                contador=imagenes.length-1
+            } 
+            sombras--
+            if (sombras==0) {
+                boxImgSecond1.classList.add("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+            }else if (sombras==1) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.add("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+            }else if (sombras==2) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.add("sombras")
+                boxImgSecond4.classList.remove("sombras")
+            }else if (sombras==3) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.add("sombras")
+            }else{
+                
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.add("sombras")
+                sombras=3
+                
+            }
+        }
+        if (fls==ConAdelante) {
+            if (contador<imagenes.length-1) {
+                img.src=imagenes[contador+1]
+                contador+1
+            }else{
+                img.src=imagenes[0]
+                contador=0
+            } 
+            sombras++
+            if (sombras==0) {
+                boxImgSecond1.classList.add("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+            }else if (sombras==1) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.add("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+            }else if (sombras==2) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.add("sombras")
+                boxImgSecond4.classList.remove("sombras")
+            }else if (sombras==3) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.add("sombras")
+            }else{
+                boxImgSecond1.classList.add("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+                sombras=0
+                
+            }
+        }
+    })
+
+    
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    let contenedor= document.querySelector('.lighbox')
+
+    carrusel(contenedor)
+}) */
+let imagenes=['img/image-product-1.jpg','img/image-product-2.jpg','img/image-product-3.jpg','img/image-product-4.jpg']
+let cont=0
+let somb=0
+function carrusel( contenedor ) {
+    contenedor.addEventListener("click",e =>{
+        let atras=contenedor.querySelector('.atras'),
+         adelante=contenedor.querySelector('.adelante'),
+         img =contenedor.querySelector('.img-main-ligh'),
+         tgt =e.target;
+        
+        if (tgt==atras) {
+            if (cont>0) {
+                img.src=imagenes[cont-1]
+                cont--   
+                imgMainLigh.classList.add("img.activa.prin") 
+            } else {
+               img.src=imagenes[imagenes.length-1] 
+               cont=imagenes.length-1 
+               imgMainLigh.classList.add("img.activa.prin") 
+            }
+            somb--
+            if (somb==0) {
+                boxImgSecond1.classList.add("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+                
+        
+            }else if (somb==1) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.add("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+                
+            }else if (somb==2) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.add("sombras")
+                boxImgSecond4.classList.remove("sombras")
+                
+            }else if (somb==3) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.add("sombras")
+                
+            }else{
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.add("sombras")
+               
+                somb=3
+            }  
+        }if (tgt==adelante) {
+            if (cont < imagenes.length-1) {
+                img.src=imagenes[cont+1]
+                cont++
+                console.log ("bien")
+                imgMainLigh.classList.add("img.activa.prin") 
+            } else {
+               img.src=imagenes[0] 
+               cont=0
+               console.log ("bien")
+               imgMainLigh.classList.add("img.activa.prin") 
+            }
+            somb++
+            if (somb==0) {
+                boxImgSecond1.classList.add("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+                
+        
+            }else if (somb==1) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.add("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+               
+            }else if (somb==2) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.add("sombras")
+                boxImgSecond4.classList.remove("sombras")
+               
+            }else if (somb==3) {
+                boxImgSecond1.classList.remove("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.add("sombras")
+               
+            } else{
+                boxImgSecond1.classList.add("sombras")
+                boxImgSecond2.classList.remove("sombras")
+                boxImgSecond3.classList.remove("sombras")
+                boxImgSecond4.classList.remove("sombras")
+                somb=0
+                
+            }   
+        }
+       
+       
+    })  
+    
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    let contenedor= document.querySelector('.lighbox')
+
+    carrusel(contenedor)
+})
+boxImgSecond1.addEventListener("click",function () {
+    imgMainLigh.setAttribute("src","img/image-product-1.jpg");
+    imgMainLigh.classList.add("img.activa.prin") 
+    boxImgSecond1.classList.add("sombras")
+    boxImgSecond2.classList.remove("sombras")
+    boxImgSecond3.classList.remove("sombras")
+    boxImgSecond4.classList.remove("sombras")
+    
+    
+})
+boxImgSecond2.addEventListener("click",function () {
+    imgMainLigh.setAttribute("src","img/image-product-2.jpg")
+    imgMainLigh.classList.add("img.activa.prin") 
+    boxImgSecond1.classList.remove("sombras")
+    boxImgSecond2.classList.add("sombras")
+    boxImgSecond3.classList.remove("sombras")
+    boxImgSecond4.classList.remove("sombras")
+    
+})
+boxImgSecond3.addEventListener("click",function () {
+    imgMainLigh.setAttribute("src","img/image-product-3.jpg");  
+    imgMainLigh.classList.add("img.activa.prin") 
+    boxImgSecond1.classList.remove("sombras")
+    boxImgSecond2.classList.remove("sombras")
+    boxImgSecond3.classList.add("sombras")
+    boxImgSecond4.classList.remove("sombras")
+    
+    
+})
+boxImgSecond4.addEventListener("click",function () {
+    imgMainLigh.setAttribute("src","img/image-product-4.jpg");  
+    imgMainLigh.classList.add("img.activa.prin") 
+    boxImgSecond1.classList.remove("sombras")
+    boxImgSecond2.classList.remove("sombras")
+    boxImgSecond3.classList.remove("sombras")
+    boxImgSecond4.classList.add("sombras")
+    
+    
+})
+
+
+
+
+
